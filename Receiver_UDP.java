b41ef99d1530f7d790a6e70574d34e559266d68b
Michael Nelson <Ren5000@Gmail.com>
2017-04-28 19:39:33 -0500

    Basically done
    Needs some quality of life stuff and probably more documentation, but the code here does the bare minimum of what it should.

--- Receiver_UDP.java
+++ Receiver_UDP.java
@@ -10,7 +10,8 @@ public class Receiver_UDP
     private final byte CONNECT_REQUEST = 0x00;
     private final byte[] CONNECT_ACK = {0x01};
     private final byte SEND_REQUEST_FLAG = 0x03;
-    private final byte SEND_REQUEST_ACK = 0x04;
+    private final byte[] SEND_REQUEST_ACK = {0x04};
+    private final byte DATA_ACK_FLAG = 0x05;
     private final byte TEARDOWN_REQUEST = 0x09;
     private final byte[] TEARDOWN_ACK = {0x0a};
 
@@ -25,7 +26,7 @@ public class Receiver_UDP
 
     private void run() throws Exception
     {
-        byte[] buffer = new byte[10000]; // Long enough for anything
+        byte[] buffer = new byte[1003]; // Long enough for anything
         DatagramPacket receivepacket = new DatagramPacket(buffer, buffer.length);
         DatagramSocket socket = new DatagramSocket(11110, InetAddress.getLocalHost()); // Creates DatagramSocket on port 11110
         socket.receive(receivepacket); // Waits for initial CONNECT_REQUEST from sender
@@ -48,9 +49,10 @@ public class Receiver_UDP
             else if (receivepacket.getData()[0] == SEND_REQUEST_FLAG)
             {
                 // Reconstruct # of packets from the 2nd two bytes received, MSB first
-                int MSB = (int)(receivepacket.getData()[1]*256);
-                int LSB = (int)(receivepacket.getData()[2]);
+                int MSB = (int)(receivepacket.getData()[1] & 0xff)*256;
+                int LSB = (int)(receivepacket.getData()[2] &0xff);
                 int numOfPackets = MSB + LSB;
+                System.out.println(numOfPackets);
 
 
                 // Extract char array at the end of SEND_REQUEST and reconstruct filename as String
@@ -60,6 +62,36 @@ public class Receiver_UDP
                     filenamechar[i - 3] = (char)receivepacket.getData()[i];
                 }
                 String filename = new String(filenamechar);
+                String append = "_received";
+                filename = filename.concat(append);
+
+                packet = new DatagramPacket(SEND_REQUEST_ACK, SEND_REQUEST_ACK.length, InetAddress.getLocalHost(), 11109);
+                socket.send(packet);
+
+
+                // Ack sent; begin receiving data packets
+                FileOutputStream received = new FileOutputStream("Received.png");
+                BufferedOutputStream bufferout = new BufferedOutputStream(received);
+                int z = 0;
+                while (z < numOfPackets)
+                {
+                    socket.receive(receivepacket);
+                    System.out.println("Size of received data packet is " + receivepacket.getData().length);
+                    bufferout.write(receivepacket.getData(), 3, receivepacket.getData().length - 3);
+                    MSB = (int)(receivepacket.getData()[1] & 0xff)*256;
+                    LSB = (int)(receivepacket.getData()[2] & 0xff);
+                    int sequencenum = MSB + LSB;
+                    byte[] dataAck = new byte[3];
+                    dataAck[0] = DATA_ACK_FLAG;
+                    dataAck[1] = (byte)(sequencenum >> 8);
+                    dataAck[2] = (byte)(sequencenum);
+                    packet = new DatagramPacket(dataAck, dataAck.length, InetAddress.getLocalHost(), 11109);
+                    System.out.println(z);
+                    z = sequencenum+1;
+                    socket.send(packet);
+                }
+                bufferout.flush();
+                bufferout.close();
             }
         }
 
--- Sender.java
+++ Sender.java
@@ -12,8 +12,7 @@ public class Sender
     private final byte DATA_FLAG = 0x02;
     private final byte SEND_REQUEST_FLAG = 0x03;
     private final byte SEND_REQUEST_ACK = 0x04;
-    private final byte FILE_RECEIVED_ACK_FLAG = 0x05;
-    private final byte RESEND_REQUEST_FLAG = 0x07;
+    private final byte DATA_ACK_FLAG = 0x05;
     private final byte[] RESEND_ACK = {0x08};
     private final byte[] TEARDOWN_REQUEST = {0x09};
     private final byte TEARDOWN_ACK = 0x0a;
@@ -125,86 +124,113 @@ public class Sender
         // Command prompt
         Scanner in = new Scanner(System.in);
         boolean teardown = false;
-        String command = "";
-        while (command.isEmpty()) // Waits for user to type SEND
+        while (!teardown)
         {
-            command = in.next();
-            if (command.equals("SEND")) {break;}
-            else if (command.equals("TEARDOWN")) {teardown = true; break;}
-            command = "";
-            System.out.println("Invalid command");
-        }
-        //Command received
-        if (teardown)
-        {
-            boolean teardownacked = false;
-            timeout = false;
-            while(!teardownacked)
+            String command = "";
+            while (command.isEmpty()) // Waits for user to type SEND
             {
-                packet = new DatagramPacket(TEARDOWN_REQUEST, TEARDOWN_REQUEST.length, InetAddress.getLocalHost(), 11110);
+                command = in.next();
+                if (command.equals("SEND")) {break;}
+                else if (command.equals("TEARDOWN")) {teardown = true; break;}
+                command = "";
+                System.out.println("Invalid command");
+            }
+            //Command received
+            if (teardown)
+            {
+                boolean teardownacked = false;
+                timeout = false;
+                while(!teardownacked)
+                {
+                    packet = new DatagramPacket(TEARDOWN_REQUEST, TEARDOWN_REQUEST.length, InetAddress.getLocalHost(), 11110);
+                    socket.send(packet);
+                    System.out.println("Teardown commenced...");
+                    socket.setSoTimeout(30*1000);
+                    try {socket.receive(receivepacket);}
+                    catch(Exception e) {continue;}
+
+                    if (receivepacket.getData()[0] == TEARDOWN_ACK) {teardownacked = true;}
+                }
+                socket.close();
+                return;
+            }
+
+
+            System.out.println("Type filename");
+            String filename = in.next();
+
+            // Gathers information on chosen file and encapsulating them in sendRequest byte array
+            File transferFile = new File(filename); // File that is being sent
+            int fileSize = (int)transferFile.length();
+            int numOfPackets = fileSize/1000;
+            if (numOfPackets%1000 != 0) {numOfPackets++;}
+            System.out.println("NumofPackets = " + numOfPackets);
+            byte[] sendRequest = new byte[3 + filename.length()];
+            sendRequest[0] = SEND_REQUEST_FLAG;
+
+            // Breaks number of packets into two bytes MSB first; assumes number of packets < 65536 (2 bytes)
+            sendRequest[1] = (byte)(numOfPackets >> 8);
+            sendRequest[2] = (byte)(numOfPackets);
+
+            // Sends filename as array of characters casted as bytes
+            char[] filenamearray = filename.toCharArray();
+            for (int i = 0; i < filename.length(); i++)
+            {
+                sendRequest[i+3] = (byte)filenamearray[i];
+            }
+            packet = new DatagramPacket(sendRequest, sendRequest.length, InetAddress.getLocalHost(), 11110);
+
+
+
+            // Waits for SEND_REQUEST_ACK
+            boolean sendRequestAcked = false;
+            timeout = false;
+            while (!sendRequestAcked)
+            {
                 socket.send(packet);
-                System.out.println("Teardown commenced...");
+                System.out.println("Sending SEND_ACK_REQUEST message...");
                 socket.setSoTimeout(30*1000);
                 try {socket.receive(receivepacket);}
-                catch(Exception e) {continue;}
+                catch(Exception e) {timeout = true; break;}
 
-                if (receivepacket.getData()[0] == TEARDOWN_ACK) {teardownacked = true;}
+                if (receivepacket.getData()[0] == SEND_REQUEST_ACK) {sendRequestAcked = true;}
             }
-            socket.close();
-            return;
+            if (timeout) {System.out.println("Timeout has occurred; exiting application"); return;}
+            System.out.println("SEND_REQUEST_ACK received; sending " + numOfPackets);
+
+
+
+            // Send request acknowledged, begin sending data packets
+            byte[] outputbuffer = new byte[1000]; // 1000 bytes per datagram as per the specification
+            FileInputStream inputStream = new FileInputStream(transferFile);
+            for (int q = 0; q < numOfPackets; q++)
+            {
+                // Constructs DATA message with header flag and packet #
+                int payloadsize = inputStream.read(outputbuffer);
+                if (payloadsize == -1) {payloadsize = 0;}
+                byte[] datapacket = new byte[3 + payloadsize];
+                datapacket[0] = DATA_FLAG;
+                datapacket[1] = (byte)(q >> 8);
+                datapacket[2] = (byte)(q);
+                for (int j = 0; j < payloadsize; j++)
+                {
+                    datapacket[j + 3] = outputbuffer[j];
+                }
+
+                packet = new DatagramPacket(datapacket, datapacket.length, InetAddress.getLocalHost(), 11110);
+                System.out.println("Size of data packet is " + datapacket.length);
+                socket.send(packet);
+                socket.receive(receivepacket);
+                if (receivepacket.getData()[0] == DATA_ACK_FLAG)
+                {
+                    int MSB = (int)(receivepacket.getData()[1] & 0xff)*256;
+                    int LSB = (int)(receivepacket.getData()[2] & 0xff);
+                    int lastacked = MSB + LSB;
+                    if (lastacked != q) {q--;}
+                }
+                else {q--;}
+            }
+            System.out.println("File sent!");
         }
-
-
-        System.out.println("Type filename");
-        String filename = in.next();
-
-        // Gathers information on chosen file and encapsulating them in sendRequest byte array
-        File transferFile = new File(filename); // File that is being sent
-        int fileSize = (int)transferFile.length();
-        int numOfPackets = fileSize/1000;
-        if (numOfPackets%1000 != 0) {numOfPackets++;}
-        byte[] sendRequest = new byte[3 + filename.length()];
-        sendRequest[0] = SEND_REQUEST_FLAG;
-
-        // Breaks number of packets into two bytes MSB first; assumes number of packets < 65536 (2 bytes)
-        sendRequest[1] = (byte)(numOfPackets >> 8);
-        sendRequest[2] = (byte)(numOfPackets);
-
-        // Sends filename as array of characters casted as bytes
-        char[] filenamearray = filename.toCharArray();
-        for (int i = 0; i < filename.length(); i++)
-        {
-            sendRequest[i+3] = (byte)filenamearray[i];
-        }
-        packet = new DatagramPacket(sendRequest, sendRequest.length, InetAddress.getLocalHost(), 11110);
-
-
-
-        // Waits for SEND_REQUEST_ACK
-        boolean sendRequestAcked = false;
-        timeout = false;
-        while (!sendRequestAcked)
-        {
-            socket.send(packet);
-            System.out.println("Sending SEND_ACK_REQUEST message...");
-            socket.setSoTimeout(30*1000);
-            try {socket.receive(receivepacket);}
-            catch(Exception e) {timeout = true; break;}
-
-            if (receivepacket.getData()[0] == SEND_REQUEST_ACK) {sendRequestAcked = true;}
-        }
-        if (timeout) {System.out.println("Timeout has occurred; exiting application"); return;}
-        System.out.println("SEND_REQUEST_ACK received; sending data now.");
-        socket.close(); // Placeholder until rest is complete
-
-
-
-        // Send request acknowledged, begin sending data packets
-        /*
-        for (int q = 0; q < numOfPackets - 1; q++)
-        {
-
-        }
-        */
     }
 }
